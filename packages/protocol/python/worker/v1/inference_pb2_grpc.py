bfaee9a521e20e9bc1264f89547b546766da85ee
Chen Yu <chenyu@Lay2-Studio.local>
2026-05-13 22:05:04 +0900

    Update Python worker dependencies

--- packages/protocol/python/worker/v1/inference_pb2_grpc.py
+++ packages/protocol/python/worker/v1/inference_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from packages.protocol.python.worker.v1 import inference_pb2 as worker_dot_v1_dot_inference__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
